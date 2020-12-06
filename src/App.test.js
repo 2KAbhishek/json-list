be15e6b5978dbf513dd8dbc4e65518d96d954835
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-12-06 21:37:44 +0530

    Add App level testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders products", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const product = screen.getByText(/Think/i);
+  expect(product).toBeInTheDocument();
 });
